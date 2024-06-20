d483a49e16c55257187b1ec93ee3be5997602554
Jeshua Maxey <me@jeshua.co>
2024-06-20 15:37:49 +0100

    auth protect app routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,33 @@
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
+const PUBLIC_ROUTES = [
+  '/',
+  '/login',
+  '/signup',
+  '/forgot-password',
+  '/reset-password',
+  '/error'
+];
+
+const API_BASE_PATH = '/api';
+
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const {user, response} = await updateSession(request);
+
+  const isAPIRoute = request.nextUrl.pathname.startsWith(API_BASE_PATH)
+  const isPublicRoute = PUBLIC_ROUTES.includes(request.nextUrl.pathname);
+
+  if(isAPIRoute) {
+    return response;
+  }
+
+  if((!user && !isPublicRoute)) {
+    const origin = new URL(request.url).origin;
+    return NextResponse.redirect(new URL(`${origin}/login`));
+  }
+
+  return response
 }
 
 export const config = {
